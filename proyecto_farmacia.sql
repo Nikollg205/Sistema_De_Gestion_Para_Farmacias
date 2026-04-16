15d08c0b8fc31a0a698b2514fff75debfde29f93
Nikollg205 <nsaraygonzalezucompensar.edu.co>
2026-04-15 22:28:12 -0500

    feat: Se agrega el script SQL de la base de datos

---
+++ proyecto_farmacia.sql
@@ -0,0 +1,316 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-04-2026 a las 05:11:18
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyecto_farmacia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_venta`
+--
+
+CREATE TABLE `detalle_venta` (
+  `id_detalle_venta` varchar(20) NOT NULL,
+  `id_factura` varchar(20) DEFAULT NULL,
+  `id_medicamento` varchar(20) DEFAULT NULL,
+  `cantidad_vendida` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura`
+--
+
+CREATE TABLE `factura` (
+  `id_factura` varchar(20) NOT NULL,
+  `fecha_factura` datetime DEFAULT NULL,
+  `precio_total` decimal(10,2) DEFAULT NULL,
+  `sub_total` decimal(10,2) DEFAULT NULL,
+  `IVA` decimal(4,2) DEFAULT NULL,
+  `estado_factura` varchar(30) DEFAULT NULL,
+  `vendedor` varchar(20) DEFAULT NULL,
+  `id_detalle_venta` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `farmacia`
+--
+
+CREATE TABLE `farmacia` (
+  `id_farmacia` varchar(20) NOT NULL,
+  `id_inventario` varchar(20) DEFAULT NULL,
+  `id_persona` varchar(20) DEFAULT NULL,
+  `nombre_farmacia` varchar(30) DEFAULT NULL,
+  `empleados` int(11) DEFAULT NULL,
+  `numero_clientes_dia` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inventario`
+--
+
+CREATE TABLE `inventario` (
+  `id_inventario` varchar(20) NOT NULL,
+  `id_lote` varchar(20) DEFAULT NULL,
+  `categoria` varchar(20) DEFAULT NULL,
+  `stock_inventario` int(11) NOT NULL,
+  `estado` varchar(20) DEFAULT NULL,
+  `numero_lotes` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lote`
+--
+
+CREATE TABLE `lote` (
+  `id_lote` varchar(20) NOT NULL,
+  `id_medicamento` varchar(20) DEFAULT NULL,
+  `id_proveedor` varchar(20) DEFAULT NULL,
+  `fecha_caducidad` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medicamento`
+--
+
+CREATE TABLE `medicamento` (
+  `id_medicamento` varchar(20) NOT NULL,
+  `tipo_medicamento` varchar(30) NOT NULL,
+  `precio` int(11) NOT NULL,
+  `unidad_medida` varchar(5) NOT NULL,
+  `formula` varchar(250) DEFAULT NULL,
+  `stock_medicamento` int(11) NOT NULL,
+  `contenido_unidad` int(11) NOT NULL,
+  `nombre_medicamento` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personas`
+--
+
+CREATE TABLE `personas` (
+  `id_persona` varchar(20) NOT NULL,
+  `nombre_persona` varchar(20) DEFAULT NULL,
+  `telefono_persona` varchar(20) DEFAULT NULL,
+  `correo_persona` varchar(50) DEFAULT NULL,
+  `fecha_registro` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `id_proveedor` varchar(20) NOT NULL,
+  `producto_proveedor` varchar(20) NOT NULL,
+  `telefono_proveedor` varchar(20) DEFAULT NULL,
+  `correo_proveedor` varchar(50) DEFAULT NULL,
+  `nombre_proveedor` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `id_rol` varchar(20) NOT NULL,
+  `nombre_rol` varchar(30) NOT NULL,
+  `descripcion_rol` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol_usuarios`
+--
+
+CREATE TABLE `rol_usuarios` (
+  `id_rol` varchar(20) NOT NULL,
+  `id_usuario` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` varchar(20) NOT NULL,
+  `id_persona` varchar(20) NOT NULL,
+  `nombre_usuario` varchar(30) DEFAULT NULL,
+  `contraseña_usuario` varchar(255) DEFAULT NULL,
+  `activo` tinyint(1) DEFAULT NULL,
+  `ultimo_acceso` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD PRIMARY KEY (`id_detalle_venta`),
+  ADD KEY `fk_detalle_venta_factura` (`id_factura`),
+  ADD KEY `fk_detalle_venta_medicamento` (`id_medicamento`);
+
+--
+-- Indices de la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD PRIMARY KEY (`id_factura`),
+  ADD KEY `fk_factura_vendedor` (`vendedor`),
+  ADD KEY `fk_factura_detalle_venta` (`id_detalle_venta`);
+
+--
+-- Indices de la tabla `farmacia`
+--
+ALTER TABLE `farmacia`
+  ADD PRIMARY KEY (`id_farmacia`),
+  ADD KEY `fk_farmacia_inventario` (`id_inventario`),
+  ADD KEY `fk_farmacia_persona` (`id_persona`);
+
+--
+-- Indices de la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  ADD PRIMARY KEY (`id_inventario`),
+  ADD KEY `fk_inventario_lote` (`id_lote`);
+
+--
+-- Indices de la tabla `lote`
+--
+ALTER TABLE `lote`
+  ADD PRIMARY KEY (`id_lote`),
+  ADD KEY `fk_lote_medicamento` (`id_medicamento`),
+  ADD KEY `fk_lote_proveedor` (`id_proveedor`);
+
+--
+-- Indices de la tabla `medicamento`
+--
+ALTER TABLE `medicamento`
+  ADD PRIMARY KEY (`id_medicamento`);
+
+--
+-- Indices de la tabla `personas`
+--
+ALTER TABLE `personas`
+  ADD PRIMARY KEY (`id_persona`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`id_proveedor`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `rol_usuarios`
+--
+ALTER TABLE `rol_usuarios`
+  ADD KEY `fk_rol_usuarios_rol` (`id_rol`),
+  ADD KEY `fk_rol_usuarios_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `username` (`nombre_usuario`),
+  ADD KEY `fk_usuario_persona` (`id_persona`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD CONSTRAINT `fk_detalle_venta_factura` FOREIGN KEY (`id_factura`) REFERENCES `factura` (`id_factura`),
+  ADD CONSTRAINT `fk_detalle_venta_medicamento` FOREIGN KEY (`id_medicamento`) REFERENCES `medicamento` (`id_medicamento`);
+
+--
+-- Filtros para la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD CONSTRAINT `fk_factura_detalle_venta` FOREIGN KEY (`id_detalle_venta`) REFERENCES `detalle_venta` (`id_detalle_venta`),
+  ADD CONSTRAINT `fk_factura_vendedor` FOREIGN KEY (`vendedor`) REFERENCES `rol` (`id_rol`);
+
+--
+-- Filtros para la tabla `farmacia`
+--
+ALTER TABLE `farmacia`
+  ADD CONSTRAINT `fk_farmacia_inventario` FOREIGN KEY (`id_inventario`) REFERENCES `inventario` (`id_inventario`),
+  ADD CONSTRAINT `fk_farmacia_persona` FOREIGN KEY (`id_persona`) REFERENCES `personas` (`id_persona`);
+
+--
+-- Filtros para la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  ADD CONSTRAINT `fk_inventario_lote` FOREIGN KEY (`id_lote`) REFERENCES `lote` (`id_lote`);
+
+--
+-- Filtros para la tabla `lote`
+--
+ALTER TABLE `lote`
+  ADD CONSTRAINT `fk_lote_medicamento` FOREIGN KEY (`id_medicamento`) REFERENCES `medicamento` (`id_medicamento`),
+  ADD CONSTRAINT `fk_lote_proveedor` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedor` (`id_proveedor`);
+
+--
+-- Filtros para la tabla `rol_usuarios`
+--
+ALTER TABLE `rol_usuarios`
+  ADD CONSTRAINT `fk_rol_usuarios_rol` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`),
+  ADD CONSTRAINT `fk_rol_usuarios_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `fk_usuario_persona` FOREIGN KEY (`id_persona`) REFERENCES `personas` (`id_persona`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

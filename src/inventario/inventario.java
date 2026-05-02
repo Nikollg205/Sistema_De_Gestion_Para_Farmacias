e4ae7b0d21e69932bde9024ace429619d116b02e
samuelRB23 <samuelcam64@gmail.com>
2026-05-01 21:14:53 -0500

    feat: se agrego en la clase inventario, el metodo buscar

--- src/inventario/inventario.java
+++ src/inventario/inventario.java
@@ -15,6 +15,7 @@ import java.util.List;
  */
 public class inventario implements CrudSimpleInterface<loteInventario> {
 
+    private inventario DAO;
     private final Conexion CON;
     private PreparedStatement ps;
     private ResultSet rs;
@@ -22,6 +23,7 @@ public class inventario implements CrudSimpleInterface<loteInventario> {
 
     public inventario() {
         CON = Conexion.getInstancia();
+        DAO = new inventario();
     }
 
     /**
@@ -214,6 +216,16 @@ public class inventario implements CrudSimpleInterface<loteInventario> {
         }
 
         return resp;
+    }
+    
+    public Medicamento buscar(String nombre){
+        List<loteInventario> lista = DAO.listar(nombre);
+        
+        if (!lista.isEmpty()) {
+            return lista.get(0).getMedicamento(); // devuelve el primero
+        }
+        
+        return null;
     }
 
     /**
